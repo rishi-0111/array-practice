cc67729a1bb9895bb697673909ee81c5b502ec0b
RESHI ARASU D <158203661+rishi-0111@users.noreply.github.com>
2026-07-18 12:04:24 +0530

    Add duplicate removal using TreeSet in Java
    Implemented a solution to remove duplicates from a sorted array using TreeSet.

--- Remove duplicates from a sorted array.java
+++ Remove duplicates from a sorted array.java
@@ -23,3 +23,29 @@ class Main{
         
     }
 }
+
+
+
+
+
+
+//  Remove duplicates from a sorted array using TreeSet
+import java.util.*;
+class Main{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+      TreeSet<Integer>t=new TreeSet<>();
+      for(int i=0;i<n;i++)
+      {
+          t.add(sc.nextInt());
+      }
+      for(int e:t)
+      {
+      System.out.print(e+" ");
+      }
+        
+    }
+}
